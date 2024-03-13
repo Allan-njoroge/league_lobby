6d861b07093504955b4b2d5a7c9389c4a6619feb
Allan-njoroge <Allandavis254@gmail.com>
2024-03-13 23:32:21 +0300

    Update

--- src/pages/CreateRoom.jsx
+++ src/pages/CreateRoom.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
 
 const CreateRoom = () => {
-  const [fullName, setFullName] = useState('');
+  const [leagueName, setLeagueName] = useState('');
   const [roomId, setRoomId] = useState('');
-
+  const [buttonClicked, setButtonClicked] = useState(false); // New state variable
 
   const generateRoomId = () => {
     // Logic to generate a unique room ID
     const numbers = [1,2,3,4,5,6,7,8,9,0];
-    //LETTERS array
     const letters = []
     for(let i = 65; i <= 90; i++) {
         letters.push(String.fromCharCode(i));
     }
 
-    if(!fullName.trim()) {
-        then(!newRoomId);
+    if(!leagueName.trim()) {
+        setButtonClicked(true); // Set buttonClicked to true
         return;
     }
-    //function to generate ID
+
     function newRoomId() {
         let roomId = '';
         const characters = [...numbers, ...letters];
@@ -35,17 +35,24 @@ const CreateRoom = () => {
   };
 
   return (
-    <div>
-      <h2>Create a League</h2>
-      <label htmlFor="fullName">Full Name:</label>
-      <input type="text" id="fullName" value={fullName} onChange={(e) => setFullName(e.target.value)} required/>
-      <button onClick={generateRoomId}>Generate Room ID</button>
-      {roomId && (
-        <div>
-          <p>Room ID: {roomId}</p>
-          <Link to={`/room-owner/${roomId}`}><button>Confirm Create Room</button></Link>
+    <div className="h-full w-full flex justify-center items-center">
+        <div className="w-full mx-auto items-center text-center grid">
+            <h1 className="text-2xl md:text-4xl text-blue-300">Create a League</h1>
+            
+            <input type="text" placeholder="League Name" id="leagueName" value={leagueName} onChange={(e) => setLeagueName(e.target.value)} required className="w-3/4 md:w-1/2 mx-auto h-8 bg-transparent border-b-2 border-blue-300 outline-none focus:outline-none px-6 my-6 text-center"/>
+            {buttonClicked && !leagueName.trim() && <p className="text-red-300">* Please enter the league name</p>}
+            <div>
+                <Button onClick={generateRoomId} className="text-xl rounded-full bg-blue-300 text-black hover:bg-transparent hover:text-blue-300 hover:border-2 hover:border-blue-300 my-6">
+                    Generate Room ID
+                </Button>
+            </div>
+            {roomId && (
+                <div>
+                <p>Room ID: {roomId}</p>
+                <Link to={`/room-owner/${roomId}`}><Button>Confirm Create Room</Button></Link>
+                </div>
+            )}
         </div>
-      )}
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="flex justify-center w-full h-full items-center">
         <div className=" items-center rounded-xl">
-            <h2 className="text-2xl md:text-4xl text-white">Welcome to <span className="text-blue-300">League</span> Lobby</h2>
+            <h2 className="text-2xl md:text-4xl">Welcome to <span className="text-blue-300">League</span> Lobby</h2>
             <div className="grid gap-5 mt-10">
                 {/* Create Room Button */}
                 <Button className="text-xl rounded-full bg-blue-300 text-black hover:bg-transparent hover:text-blue-300 hover:border-2 hover:border-blue-300">
